6a9491ebca86a0947428d369ccf4c2d71e14abf7
Suraj Gaud <suraj.ug@gmail.com>
2015-10-07 15:57:28 -0500

    changed the orange file to turkey

--- turkey.py
+++ turkey.py
@@ -1,39 +1,22 @@
 __author__ = 'Suraj'
 
 import numpy as np
-from numpy import *
-from scipy import signal
+from scipy import signal,misc
 import matplotlib.pyplot as plt
+image = misc.lena().astype(np.float32)
+derfilt = np.array([1.0, -2, 1.0], dtype=np.float32)
+ck = signal.cspline2d(image, 8.0)
+deriv = (signal.sepfir2d(ck, derfilt, [1]) +
+         signal.sepfir2d(ck, [1], derfilt))
 
-#signal 1
-A1 = 10.0
-F1 = 2.0
-
-#signal 2
-A2 = 0.1
-F2 = 20.0
-
-t = np.arange(0.0,10.0,0.01)
-
-S1 = A1*np.sin(F1,t)
-S2 = A2*np.sin(F2,t)
-#w1 = (2.*np.pi*F1)   #omega 1
-#w2 = (2.*np.pi*F2)   #omega 2
-#S1 = A1*np.sin(w1*t+np.pi/2) #signal 1
-#S2 = A2*np.sin(w2*t+np.pi/2) #signal 3
-S3 = signal.convolve(S1,S2,mode='same') # convolved signal 3
-a=zeros(shape=(1000,))
-a=S3[999:1000]
-print len(S1)
-print len(S2)
-
-
-plt.subplot(3,1,1)
-plt.plot(t,S1)
-plt.subplot(3,1,2)
-plt.plot(t,S2)
-plt.subplot(3,1,3)
-plt.plot(t,S3)
+plt.figure()
+plt.imshow(image)
+plt.gray()
+plt.title('Original image')
 plt.show()
 
-
+plt.figure()
+plt.imshow(deriv)
+plt.gray()
+plt.title('Output of spline edge filter')
+plt.show()
